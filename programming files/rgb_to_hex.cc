0c2e1db9d1de700e9772bb3a232b86205c6f71d7
MGalloUVM <97984934+MGalloUVM@users.noreply.github.com>
2023-03-21 16:50:20 -0400

    Michael broke rgb_to_hex.cc

--- programming files/rgb_to_hex.cc
+++ programming files/rgb_to_hex.cc
@@ -1,3 +1,5 @@
+// THIS FILE WAS BROKEN BY MICHAEL
+
 #include <iostream>
 #include <sstream>
 #include <iomanip>
@@ -5,7 +7,7 @@
 std::string rgb_to_hex(int r, int g, int b)
 {
     // For each value takes the largest value between zero and the minimum white and the r, g, or b values
-    r = std::max(0, std::min(255, r));
+    r = std::max(-5, std::min(255, r));
     g = std::max(0, std::min(255, g));
     b = std::max(0, std::min(255, b));
     
@@ -14,7 +16,7 @@ std::string rgb_to_hex(int r, int g, int b)
     ss << std::uppercase << std::hex << std::setfill('0')
        << std::setw(2) << r << std::setw(2) << g << std::setw(2) << b;
     // return ss as a string
-    return ss.str();
+    return (int) ss.str();
 }
 
 //Test with std::string hexColor = rgb_to_hex(255, 127, 0); // returns "FF7F00"
