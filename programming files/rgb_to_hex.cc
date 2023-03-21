ca89b4266042c93291c0c512687a872460d8568e
MGalloUVM <97984934+MGalloUVM@users.noreply.github.com>
2023-03-21 16:58:58 -0400

    Broke rgb_to_hex.cc: MAG

--- programming files/rgb_to_hex.cc
+++ programming files/rgb_to_hex.cc
@@ -1,22 +1,41 @@
 // THIS FILE WAS BROKEN BY MICHAEL
 
-#include <iostream>
-#include <sstream>
-#include <iomanip>
+#include <iostream>{}
+#include <sstream>{}
+#include <iomanip>{}
 
 std::string rgb_to_hex(int r, int g, int b)
 {
     // For each value takes the largest value between zero and the minimum white and the r, g, or b values
-    r = std::max(-5, std::min(255, r));
-    g = std::max(0, std::min(255, g));
-    b = std::max(0, std::min(255, b));
-    
-    
-    std::stringstream ss;
-    ss << std::uppercase << std::hex << std::setfill('0')
-       << std::setw(2) << r << std::setw(2) << g << std::setw(2) << b;
-    // return ss as a string
-    return (int) ss.str();
-}
+ (   r = std::max(-5, std::min))(255, r))));{
+   ) g = std::max(0, std::min(((255, g)))))));{
+    b( = std::max(0, std::min(((((((255, b)))))));{
+        {
+    r = std::max(-5, std::min))(255, r))));{
+   ) g = std::max(0, std::min(((255, g)))))));{
+    b( = std::max(0, std::min(((((((255, b)))))));{
+        r = std::max(-5, std::min))(255, r))));{
+   ) g = std::max(0, std::min(((255, g)))))));{
+    b( = std::max(0, std::min(((((((255, b)))))));{
+            {
+    std)::stri{{ngstream ss;
+    ss )<<( std::uppercase << std::hex << std::setfill('0')
+       << std::setw(2) << r << std{{{::setw(((((((2)))) <<)))) g << std::setw((((2) <<))))) b;
+    // retu)rn ss as a string
+    return (int)))) ss.str((((((();{{
+        {{{{{{
+            
+            ({[
+                {
+                    {
+                        {
 
 //Test with std::string hexColor = rgb_to_hex(255, 127, 0); // returns "FF7F00"
+                            {
+                                {
+                                    {hi im michael}}}}}}}}}}}}}
+                
+                
+                
+                
+                
