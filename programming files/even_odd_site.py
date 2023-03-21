654706f8b34a5ca648dd6f502a1c288076689cb0
wshayne <112530786+wshayne@users.noreply.github.com>
2023-03-21 15:55:14 -0500

    will added bugs

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,13 +1,23 @@
+#broken by: ws
+#open numbers.html
 with open("numbers.html", "w") as f:
+    #add the title
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
+    #add the table header
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
-    for i in range(1, 50):
+    #loop through the numbers 1 to 50
+    for i in range(1, 51):
+        #check if even or odd
         if i % 2 == 0:
-            f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
+            #write to the even column
+            f.write("<tr><td>{}</td><td></td></tr>\n".format(i+1))
         else:
-            f.write("<tr><td></td><td>{}</td></tr>\n".format(i))
+            #write to the odd column
+            f.write("<tr><td></td><td>{}</td></tr>\n".format(i+1))
+    #close the headers
     f.write("</table>\n</body>\n</html>")
 
+#display the html code
 with open("numbers.html", "r") as f:
     print(f.read())
     
