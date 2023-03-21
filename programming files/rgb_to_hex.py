5df01adfaa5a58b8b380bf67a72a4b389d23ded6
wshayne <112530786+wshayne@users.noreply.github.com>
2023-03-21 16:30:28 -0500

    Fixed minimum r and g values to be 0

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -2,8 +2,8 @@
 def rgb_to_hex(r, g):
     # Insert the r,g, and b values into the function
     # For each, take the max of 0 and the min value between white and the value
-    r = max(255, min(255, r))
-    g = max(255, min(255, g))
+    r = max(0, min(255, r))
+    g = max(0, min(255, g))
     b = max(0, min(255, b))
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
