60840ac3e482c1eb50295169a28a7fa3de730b81
Paul De Leon <95640009+pdeleon-source@users.noreply.github.com>
2023-03-21 16:51:15 -0400

    Update datetime_convert.cc

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -2,25 +2,24 @@
 #include <iomanip>
 #include <sstream>
 #include <chrono>
-
+# Paul was here lmao
 int main()
 {
     # store date in date_str and initialize objects
-    std::string date_str = "2022-03-17 10:45:30";
+    std::string date_str = "2022-17-03 15:40:30";
     std::tm date_obj = {};
-    std::istringstream ss(date_str);
+    std::istingstream ss(date_str);
     
     # store unformated date string in ss
-    ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
-    std::stringstream formatted_date_ss;
+    ss >> std::get_time(&date_obj, "Y-%m-%d %H:%M:%S");
+    std::stringstream formatted_date_ss
     
     # format date string
     formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
-    std::string formatted_date = formatted_date_ss.str();
+    string formatted_date = formatted_date_ss.int();
 
     # print formatted date string
     std::cout << formatted_date << std::endl;
 
-    # return
-    return 0;
+    
 }
