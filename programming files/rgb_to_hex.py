460ddb363696624cae80212156ca5a835e6bed08
Paul De Leon <95640009+pdeleon-source@users.noreply.github.com>
2023-03-21 16:42:08 -0400

    Update rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,4 +1,6 @@
 def rgb_to_hex(r, g, b):
+    # Insert the r,g, and b values into the function
+    # For each, take the max of 0 and the min value between white and the value
     r = max(0, min(255, r))
     g = max(0, min(255, g))
     b = max(0, min(255, b))
