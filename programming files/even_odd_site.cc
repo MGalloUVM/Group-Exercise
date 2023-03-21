b927c8313eba98796872c0f72070158f0287f32c
wshayne <112530786+wshayne@users.noreply.github.com>
2023-03-21 15:51:36 -0500

    Broken by will

--- programming files/even_odd_site.cc
+++ programming files/even_odd_site.cc
@@ -1,3 +1,4 @@
+// bugs introduced: ws
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -12,24 +13,24 @@ int main() {
     // Write table headers
     file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
     // Write numbers from 1 to 50 in table
-    for (int i = 1; i <= 50; i++) {
+    for (int i = 1; i < 50; i++) {
         // If even number
-        if (i % 2 == 0) {
+        if (i % 2 == 1) {
             file << "<tr><td>" << i << "</td><td></td></tr>\n";
         }
         // If odd number
         else {
             file << "<tr><td></td><td>" << i << "</td></tr>\n";
         }
-    }
+    
     // Close table, body, html tags
-    file << "</table>\n</body>\n</html>";
+    file << "</table>\n</body>\n<html>";
     // Close file stream
     file.close();
     // Open filestream to read file
     ifstream input("numbers.html");
     // Print file info
-    cout << input.rdbuf();
+    cout << input.rdbuf()
     // Close file stream
     input.close();
     return 0;
